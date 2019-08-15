8bf28f469e156b2076727f3564d1d119c40f2c68
Shola Ayeni <ayenisholah@yahoo.com>
2019-08-15 23:35:19 +0100

    useEffect hooks in action

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,40 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-// class ResourceList extends React.Component {
-//   state = {
-//     resources: [],
-//   }
+const ResourceList = ({ resourceName }) => {
 
-//   async componentDidMount() {
-//     const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resourceName}`);
+  const [ resources, setresources ] = useState([]);
+  const fetchResources = async (resourceName) => {
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/${resourceName}`);
+    setresources(response.data);
+  }
 
-//     this.setState({ resources: response.data });
-//   }
+  useEffect(() => {
+    fetchResources(resourceName)
+  }, [resourceName])
 
-//   async componentDidUpdate(prevProps) {
-//     if(prevProps.resourceName !== this.props.resourceName) {
-//       const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resourceName}`);
-
-//       this.setState({ resources: response.data });
-//     }
-//   }
-
-//   render() {
-//     return (
-//       <ul>
-//         {this.state.resources.map(resource => (
-//           <li key={resource.id}>{resource.title}</li>
-//         ))}
-//       </ul>
-//     )
-//   }
-// }
-
-const ResourceList = () => {
   return (
     <ul>
-      {this.state.resources.map(resource => (
+      {resources.map(resource => (
         <li key={resource.id}>{resource.title}</li>
       ))}
     </ul>
